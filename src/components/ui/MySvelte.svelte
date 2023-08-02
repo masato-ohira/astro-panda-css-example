ed466af2f1fe75d7f0fe074114a0da64e1005767
Masato Ohira <ohira.ekzm@gmail.com>
2023-08-02 10:49:13 +0900

    fix

--- src/components/ui/MySvelte.svelte
+++ src/components/ui/MySvelte.svelte
@@ -1,26 +1,56 @@
-<script lang='ts'>
-import {css} from '@@/styled-system/css'
-import MdTagFaces from 'svelte-icons/md/MdTagFaces.svelte'
-import { center } from '@@/styled-system/patterns'
-const name: string = 'This is svelte'
+<script lang="ts">
+  import { css } from '@@/styled-system/css'
+  import MdTagFaces from 'svelte-icons/md/MdTagFaces.svelte'
+  import { center, grid } from '@@/styled-system/patterns'
+  import { take } from 'lodash-es'
 
+  const name: string = 'This is svelte'
+
+  type TodoType = {
+    userId: number
+    id: number
+    title: string
+    completed: boolean
+  }
+
+  const fetchData = async (url: string): Promise<TodoType[]> => {
+    const res = await fetch(url)
+    const json = await res.json()
+    return take(json, 16)
+  }
 </script>
 
-<div class={center({
-  minH: '50vh',
-})}>
-  <div class={css({
-    bgColor: 'red.50',
-    color: 'red.800',
-    py: 6,
-    px: 32,
-    fontWeight: '500'
-  })}>
-  <div class={css({
-    w: '100px'
-  })}>
-    <MdTagFaces />
-  </div>
-    <p>{name}</p>
-  </div>
+<div>
+  {#await fetchData('https://jsonplaceholder.typicode.com/todos')}
+    <p>ロード中...</p>
+  {:then items}
+    <div
+      class={grid({
+        gridTemplateColumns: 'repeat(4, 1fr)',
+        gap: 8,
+      })}
+    >
+      {#each items as item}
+        <div>
+          <img
+            class={css({
+              display: 'block',
+              width: '100%',
+              height: 'auto',
+            })}
+            src={`https://placehold.jp/eeeeee/222222/400x400.png?text=${item.id}`}
+            alt=""
+          />
+          <p
+            class={css({
+              pt: 3,
+              lineHeight: 1.4,
+            })}
+          >
+            {item.title}
+          </p>
+        </div>
+      {/each}
+    </div>
+  {/await}
 </div>
